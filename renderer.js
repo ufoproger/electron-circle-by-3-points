f4d5f6aacad81cea436b188e0f9b968ea4729c55
Mikhail Snetkov <ufoproger@gmail.com>
2017-12-22 00:32:06 +0700

    Пробные наработки

--- renderer.js
+++ renderer.js
@@ -1,7 +1,3 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 let THREE = require('three')
 let OrbitControls = require('three-orbit-controls')(THREE)
 
@@ -11,11 +7,26 @@ let geometry, material, mesh
 init()
 animate()
 
+function addLine () {
+  let material = new THREE.LineBasicMaterial({
+    color: 0x0000ff,
+    linewidth: 3
+  })
+
+  let geometry = new THREE.Geometry()
+
+  geometry.vertices.push(new THREE.Vector3(-0.2, 0, 0))
+  geometry.vertices.push(new THREE.Vector3(0, 0.5, 0))
+  geometry.vertices.push(new THREE.Vector3(0.4, 0, 0))
+
+  let line = new THREE.Line(geometry, material)
+
+  scene.add(line)
+}
+
 function init () {
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
-  camera.position.x = 1
-  camera.position.y = 0.5
-  camera.position.z = 0.5
+  camera.position.copy(new THREE.Vector3(1, 0.5, 0.5))
 
   scene = new THREE.Scene()
   controls = new OrbitControls(camera)
@@ -25,12 +36,14 @@ function init () {
   mesh = new THREE.Mesh(geometry, material)
   scene.add(mesh)
 
-  scene.add(new THREE.GridHelper(10, 100))
+  scene.add(new THREE.GridHelper(4, 4 * 10))
 
   let axes = new THREE.AxesHelper(5)
 
   axes.material.linewidth = 2
   scene.add(axes)
+
+  addLine()
 
   controls.update()
   renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -41,9 +54,8 @@ function init () {
 function animate () {
   window.requestAnimationFrame(animate)
 
-  // mesh.rotation.x += 0.01
+  mesh.rotation.x += 0.01
   mesh.rotation.y += 0.005
-
   controls.update()
   renderer.render(scene, camera)
 }
